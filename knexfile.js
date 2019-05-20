dee27b70c494d6b621b7e9a20051a5fc88cdf0ce
Timothy Shores <timothymshores@gmail.com>
2019-05-20 13:32:37 -0500

    Update knexfile.js with development, testing and production environments

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,38 @@
-// Update with your config settings.
-
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    development: {
+        client: 'sqlite3',
+        useNullAsDefault: true,
+        connection: {
+            filename: './data/data.db3',
+        },
+        migrations: {
+            directory: './data/migrations',
+        },
+        seeds: {
+            directory: './data/seeds',
+        },
     },
-    pool: {
-      min: 2,
-      max: 10
+    testing: {
+        client: 'sqlite3',
+        useNullAsDefault: true,
+        connection: {
+            filename: './data/test.db3',
+        },
+        migrations: {
+            directory: './data/migrations',
+        },
+        seeds: {
+            directory: './data/seeds',
+        },
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            directory: './data/migrations',
+        },
+        seeds: {
+            directory: './data/seeds',
+        },
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
