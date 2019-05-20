1bb62f1d13aebf91a40e0f114cfe4481b8fe0c99
Timothy Shores <timothymshores@gmail.com>
2019-05-20 17:56:39 -0500

    Create and update suggestions table migration

---
+++ data/migrations/20190520174335_suggestions.js
@@ -0,0 +1,19 @@
+exports.up = function (knex, Promise) {
+    return knex.schema
+        .createTable('suggestions', tbl => {
+            tbl.increments();
+            tbl.integer('user_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('users')
+                .onDelete('RESTRICT')
+                .onUpdate('CASCADE');
+            tbl.float('score');
+        })
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema
+        .dropTableIfExists('suggestions')
+};
